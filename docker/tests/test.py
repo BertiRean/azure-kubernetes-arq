7c4d251f7e514d2db0020393581a917736edd4ae
BertiRean <tplaza15@gmail.com>
2023-09-27 23:42:03 -0300

    Finishing Test of Python Script

--- docker/tests/test.py
+++ docker/tests/test.py
@@ -1,64 +1,92 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_date, month, avg, when, regexp_replace, expr
 from pyspark.sql.functions import col
 
+# Crear Sesion de Spark
 spark = SparkSession.builder.master("yarn").appName("Arq-App").config(
-  map={
-    "spark.sql.legacy.timeParserPolicy" : "LEGACY",
-    "spark.submit.deployMode" : "client"
-  }
-).getOrCreate()
+    map={
+     "spark.sql.legacy.timeParserPolicy" : "LEGACY",
+     "spark.submit.deployMode" : "client"
+    }).getOrCreate()
 
 #Se lee el archivo
+try:  
+    print("Se procede a leer el archivo")
+    df = spark.read.csv("TGN_Entrega_GBA_Agosto.csv", header="true",sep=";")
 
-df = spark.read.csv("TGN_Entrega_GBA_Agosto.csv", header="true",sep=";")
-df.show(10)
+    print("Archivo leído con éxito")
+    df.show(10)
 
-# Volumen Nominal Total por Mes
+    # -- CASOS DE USO --
 
-# Convertir la columna 'fecha' al formato de fecha correcto
+    # 1) Volumen Nominado por mes
 
-df1 = df.withColumn('Fecha',when(expr("substring(Fecha, 5, 1) = '/'"), to_date('Fecha', 'dd/MM/yyyy')).otherwise(to_date('Fecha', 'MM/dd/yyyy')))
+    print("Caso de Uso 1:")
 
-# Reemplazar comas en 'volumen nominado' y convertir a tipo numérico
+    from pyspark.sql.functions import to_date, month, avg, sum, col, when, regexp_replace, expr
 
-df2 = df1.withColumn('Volumen Nominado (miles m3)',regexp_replace('Volumen Nominado (miles m3)', ',', '').cast('double'))
+    # Convertir la columna 'fecha' al formato de fecha correcto
+    df1 = df.withColumn('Fecha', when(expr("substring(Fecha, 5, 1) = '/'"), to_date('Fecha', 'dd/MM/yyyy')).otherwise(to_date('Fecha', 'MM/dd/yyyy')))
 
-# Reemplazar nulos en 'volumen nominado' con 0
+    # Reemplazar comas en 'Volumen Nominado (miles m3)' y convertir a tipo numérico
+    df1 = df1.withColumn('Volumen Nominado (miles m3)', regexp_replace('Volumen Nominado (miles m3)', ',', '').cast('double'))
 
-df2 = df2.withColumn('Volumen Nominado (miles m3)', when(col('Volumen Nominado (miles m3)').isNull(), 0).otherwise(col('Volumen Nominado (miles m3)')))
+    # Reemplazar nulos en 'Volumen Nominado (miles m3)' con 0
+    df1 = df1.withColumn('Volumen Nominado (miles m3)', when(col('Volumen Nominado (miles m3)').isNull(), 0).otherwise(col('Volumen Nominado (miles m3)')))
 
-result = df2.groupBy(month('Fecha').alias('mes')).agg(avg('Volumen Nominado (miles m3)').alias('volumen_nominado_promedio'),sum(col('Volumen Nominado (miles m3)').cast('float')).alias('volumen_nominado_total'))
+    # Agregar una columna adicional para identificar valores no numéricos
+    df1 = df1.withColumn('EsNumerico', when(df1['Volumen Nominado (miles m3)'].cast('string').rlike('^[0-9]+\\.?[0-9]*$'), 1).otherwise(0))
 
-print("Volumen Nominado Total por mes")
-result.show()
+    # Filtrar registros donde la columna no es numérica
+    df1 = df1.filter(df1['EsNumerico'] == 1)
 
-# Calcular la sumatoria de volúmenes nominados por Punto de Entrega y Gasoducto
+    result = df1.groupBy(month('Fecha').alias('mes')).agg(
+        avg('Volumen Nominado (miles m3)').alias('volumen_nominado_promedio'),
+        sum('Volumen Nominado (miles m3)').alias('volumen_nominado_total')
+    )
 
-result2 = df2.groupBy('Punto de Entrega','Gasoducto').agg(sum('Volumen Nominado (miles m3)').alias('sumatoria_volumen_nominado'))
-
-print("Volumen Nominado Total: Por Gasoducto y Puntos de Entrega")
-result2.show(10)
+    print("Volumen Nominado Total por mes")
+    result.show()
 
 
-# Calcular la sumatoria de volúmenes autorizados por Punto de Entrega y Gasoducto
+    # 2) Calcular la sumatoria de volúmenes nominados por Punto de Entrega y Gasoducto
 
-# Reemplazar comas en 'volumen nominado' y convertir a tipo numérico
-df3 = df2.withColumn('Volumen Autorizado (miles m3)',regexp_replace('Volumen Autorizado (miles m3)', ',', '').cast('double'))
+    print("Caso de Uso 2:")
 
-# Reemplazar nulos en 'volumen nominado' con 0
-df3 = df3.withColumn('Volumen Autorizado (miles m3)', when(col('Volumen Autorizado (miles m3)').isNull(), 0).otherwise(col('Volumen Autorizado (miles m3)')))
+    result2 = df1.groupBy('Punto de Entrega','Gasoducto').agg(sum('Volumen Nominado (miles m3)').alias('sumatoria_volumen_nominado'))
 
-result3 = df3.groupBy('Punto de Entrega','Gasoducto').agg(sum('Volumen Autorizado (miles m3)').alias('sumatoria_volumen_autorizado'))
+    print("Volumen Nominado Total: Por Gasoducto y Puntos de Entrega")
+    result2.show(10)
 
-print("Volumen Autorizado Total: Por Gasoducto y Puntos de Entrega")
-result3.show()
 
-# Calcular la cantidad de entregas por cargador
+    # 3) Calcular la sumatoria de volúmenes autorizados por Punto de Entrega y Gasoducto
 
-from pyspark.sql.functions import count
+    print("Caso de Uso 3:")
 
-result4 = df3.groupBy('Cargadores').agg(count('Fecha').alias('cantidad_entregas'))
+    # Reemplazar comas en 'volumen nominado' y convertir a tipo numérico
+    df3 = df1.withColumn('Volumen Autorizado (miles m3)',regexp_replace('Volumen Autorizado (miles m3)', ',', '').cast('double'))
 
-print("Cantidad de Entregas por Cargador")
-result4.show()
+    # Reemplazar nulos en 'volumen nominado' con 0
+    df3 = df3.withColumn('Volumen Autorizado (miles m3)', when(col('Volumen Autorizado (miles m3)').isNull(), 0).otherwise(col('Volumen Autorizado (miles m3)')))
+
+    result3 = df3.groupBy('Punto de Entrega','Gasoducto').agg(sum('Volumen Autorizado (miles m3)').alias('sumatoria_volumen_autorizado'))
+
+    print("Volumen Autorizado Total: Por Gasoducto y Puntos de Entrega")
+    result3.show()
+
+
+    # 4) Calcular la cantidad de entregas por cargador
+
+    print("Caso de Uso 4:")
+
+    from pyspark.sql.functions import count
+
+    result4 = df3.groupBy('Cargadores').agg(count('Fecha').alias('cantidad_entregas'))
+
+    print("Cantidad de Entregas por Cargador")
+    result4.show()
+
+except Exception as e:
+    print(e)
+finally:
+    spark.stop()
+    print("Exiting job")
